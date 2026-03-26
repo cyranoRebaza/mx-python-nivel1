51ead23693731d97ca18c14748b1895131de2507
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-03-26 15:09:29 -0300

    docs: unidad 02 - tipos de datos simples

---
+++ 01_teoria/unidad_02/02_tipos_datos.py
@@ -0,0 +1,69 @@
+#============================================
+# unidad 02: tipo de datos simples
+#============================================ 
+
+# numero enteros (int)
+#   - son numeros sin parte decimal positivos o negativos
+#   - puedo realizar las operacion basicas
+
+
+#  numeros decimales (float)
+#   - son numeros que contienen una parte fraccionaria separada por un punto
+#   - equivalen a los numeros racionales
+
+
+# cadena de caracteres (str)
+#   - secuencia de caracteres para almacenar datos textuales
+
+
+# Booleano (bool)
+#   - representan dos valores: verdadero(True) y falso(False)
+
+#-------------------------------------------------------------------------
+
+# tipado dinamico: no necesita declarar el tipo
+variable = 5      # python entiende que es un int
+variable = "hola" # python interpreta como str
+
+# numero enteros (int)
+edad = 25
+numero_de_estrella = -100
+anio = 2026
+
+print(edad)
+print(numero_de_estrella)
+print(anio)
+
+#  numeros decimales (float)
+altura = 1.75
+temperatura = -20.3
+precio = 19.99
+
+print(altura)
+print(temperatura)
+print(precio)
+
+# cadena de caracteres (str)
+# usar comillas simples o dobles o f-string para mezclar
+nombre = "Juan"
+mensaje = "Hola Mundo"
+letra = 'A'
+
+print(nombre)
+print(mensaje)
+print(letra)
+
+edad = 30
+print(f"{mensaje}, tengo {edad} anios y mi nombre es {nombre}")
+
+# Booleano (bool)
+es_mayor_de_edad = True
+examen_aprobado = False
+
+print(es_mayor_de_edad)
+print(examen_aprobado)
+
+
+
+
+
