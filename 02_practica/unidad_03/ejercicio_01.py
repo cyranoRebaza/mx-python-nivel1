a6e4d21b380c21868833aed8841a91cf707b635a
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-04-24 14:24:37 -0300

    feat: unidad 03 - agrega ejercicio 01 resuelto

---
+++ 02_practica/unidad_03/ejercicio_01.py
@@ -0,0 +1,17 @@
+"""
+Unidad 03 - Ejercicio 01
+
+Hacer un programa para ingresar un número y luego se emita por pantalla un
+cartel aclaratorio si “Es mayor a 10” o “No es mayor a 10”.
+
+"""
+
+# Pedir datos
+numero = int(input("Ingrese un numero: "))
+
+# Calcular y mostrar
+if numero > 10:
+    print("Es mayor a 10")
+
+else:
+    print("No es mayor a 10")
