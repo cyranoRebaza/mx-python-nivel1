ba1b56053a7a4d058b9436d97b1bf1b2135dcf61
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-04-24 15:26:59 -0300

    feat: unidad 03 - agrega ejercicio 03 resuelto

---
+++ 02_practica/unidad_03/ejercicio_03.py
@@ -0,0 +1,22 @@
+"""
+Unidad 03 - Ejercicio 03
+
+Hacer un programa para ingresar dos números y que luego emita por pantalla el
+mayor de ellos o un cartel aclaratorio “Son iguales” en el caso de que así sea.
+
+Nota: los números pueden ser iguales.
+
+"""
+# Pedir datos
+numero_1 = int(input("Ingrese primero numero: "))
+numero_2 = int(input("Ingrese segundo numero: "))
+
+# Calcular y mostrar
+if numero_1 == numero_2:
+    print("Son iguales")
+
+else:
+    if numero_1 > numero_2:
+        print(f"El mayor es: {numero_1}")
+    else:
+        print(f"El mayor es: {numero_2}")
