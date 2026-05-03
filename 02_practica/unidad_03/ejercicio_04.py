d2c08724efa8deecad6e96f73bae70b21da84527
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-05-03 09:43:48 -0300

    feat: unidad 03 - agrega ejercicio 04 resuelto

---
+++ 02_practica/unidad_03/ejercicio_04.py
@@ -0,0 +1,24 @@
+"""
+Unidad 03 - Ejercicio 04
+
+Hacer un programa para ingresar un número y luego se emita un cartel por
+pantalla “Positivo” si el número es mayor a cero, “Negativo” si el número es
+menor a cero o “Cero” si el número es igual a cero.
+
+------------------------------------
+criterio: buscamos directamente el mayor y porque son solo dos numeros
+
+"""
+
+# Pedir los datos
+numero = int(input("Ingrese un numero: "))
+
+# calcular y mostrar
+if numero > 0:
+    print("POsitivo")
+
+if numero == 0:
+    print("Cero")
+    
+if numero < 0:
+    print("Negativo")
