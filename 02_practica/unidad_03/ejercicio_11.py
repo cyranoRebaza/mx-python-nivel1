0ed95a9f80e2c2eccc16e5b9b42a21b003f75d55
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-05-09 02:24:03 -0300

    feat: unidad 03 - agrega ejercicio 11 resuelto

---
+++ 02_practica/unidad_03/ejercicio_11.py
@@ -0,0 +1,31 @@
+"""
+Unidad 03 - Ejercicio 11
+
+Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+cuántos son menores a 100.
+
+"""
+
+# Pedir datos
+numero_1 = int(input("Ingrese un numero: "))
+numero_2 = int(input("Ingrese un numero: "))
+numero_3 = int(input("Ingrese un numero: "))
+numero_4 = int(input("Ingrese un numero: "))
+
+# Buscar  cuantos son menores a 100
+menores_100 = 0
+
+if numero_1 < 100:
+    menores_100 = menores_100 + 1
+
+if numero_2 < 100:
+    menores_100 = menores_100 + 1
+    
+if numero_3 < 100:
+    menores_100 = menores_100 + 1
+    
+if numero_4 < 100:
+    menores_100 = menores_100 + 1
+    
+# Mostrar cuantos son menores a 100
+print(f"La cantidad de numeros menores a 100 son: {menores_100}")
