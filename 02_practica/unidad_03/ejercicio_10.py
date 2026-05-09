4e0c5261a1a93da15000d8bf0afd34f894c957bd
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-05-09 02:17:44 -0300

    feat: unidad 03 - agrega ejercicio 10 resuelto

---
+++ 02_practica/unidad_03/ejercicio_10.py
@@ -0,0 +1,26 @@
+"""
+Unidad 03 - Ejercicio 10
+
+Hacer un programa para ingresar cuatro números y luego mostrar por pantalla
+cuáles son mayores a 100.
+
+"""
+
+# Pedir datos
+numero_1 = int(input("Ingrese un numero: "))
+numero_2 = int(input("Ingrese un numero: "))
+numero_3 = int(input("Ingrese un numero: "))
+numero_4 = int(input("Ingrese un numero: "))
+
+# Buscar y mostrar los numeros mayores a 100
+if numero_1 > 100:
+    print(numero_1)
+
+if numero_2 > 100:
+    print(numero_2)
+    
+if numero_3 > 100:
+    print(numero_3)
+    
+if numero_4 > 100:
+    print(numero_4)
