7a86307fc874449b5d12200cf84f2822747d7089
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-05-12 00:13:38 -0300

    feat: unidad 03 - agrega ejercicio 13 resuelto

---
+++ 02_practica/unidad_03/ejercicio_13.py
@@ -0,0 +1,23 @@
+"""
+Unidad 03 - Ejercicio 13
+
+Hacer un programa que solicite el ingreso de un número y luego emita un cartel
+por pantalla aclarando si el mismo es múltiplo de 5.
+
+"""
+# Pedir datos
+numero = int(input("Ingrese un numero: "))
+
+# Calcular y mostrar
+if numero % 5 == 0:
+    print(f"El numero {numero} es multiplo de 5")
+    
+else:
+    print(f"El numero {numero} no es multiplo de 5")
+
+
+    
+
+
+
+    
