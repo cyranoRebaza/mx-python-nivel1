c92175f531479077ce58f492d9dbe6102948ae66
cyranoRebaza <144959092+cyranoRebaza@users.noreply.github.com>
2026-06-09 01:42:36 -0300

    feat: unidad 04 - agrega ejercicio 01 resuelto

---
+++ 02_practica/unidad_04/ejercicio_01.py
@@ -0,0 +1,20 @@
+"""
+Unidad 04 - Ejercicio 01
+
+Hacer un programa para mostrar los números del 1 al 10. No se debe realizar 
+ningún pedido de datos. 
+
+Nota: probalo tanto con For como con While.
+
+"""
+
+# usando for
+for numero in range(1, 11):
+    print(numero)
+
+print()
+# usando while
+numero_2 = 1
+while numero_2 <= 10:
+    print(numero_2)
+    numero_2 += 1
